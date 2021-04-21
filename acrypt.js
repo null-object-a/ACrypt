d3aa605570867c18b1cecfd95d78a9110ac0ef57
Alfonso <68069359+null-object-a@users.noreply.github.com>
2021-04-21 20:04:16 +0200

    Add files via upload

---
+++ acrypt.js
@@ -0,0 +1,55 @@
+String.prototype.reverse=function() {return this.split("").reverse().join("");}
+ function Sum(key) {
+        var result = 0
+        for (var x = 0; x < key.length; x++)
+        {
+            var c = key.charAt(x);
+            result += c.charCodeAt(0);
+        }
+        return result
+    }
+ function EncodeA(text,key) {
+        var result = []
+        for (var x = 0; x < text.length; x++)
+        {
+            var c = text.charAt(x);
+            result.push(c.charCodeAt(0) * Sum(key))
+        }
+        return result.join("/").reverse()
+    }
+ function DecodeA(text,key) {
+        var result = ""
+        var t = text.reverse().split('/')
+        //console.log(t)
+        for (let i of t)
+        {
+           result += String.fromCharCode(parseInt(i)/Sum(key));
+        }
+        //console.log("DECODEA "+result)
+        return result
+    }
+  function  encrypt(text,key){
+        try {
+		    var tmp = EncodeA(text,key)
+            console.log(tmp)
+            tmp = "[ACRYPT] "+tmp.reverse()
+		    return tmp;
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+    }
+
+  function  decrypt(text,key){
+        try {
+            if (text.startsWith("[ACRYPT]")) {
+                var tmp = text.replace("[ACRYPT] ","").reverse();
+                console.log(DecodeA(tmp,key))
+                return DecodeA(tmp,key)
+            }
+            return ""
+        } catch(err) {
+            return err;
+        }
+    }
+    console.log(encrypt("anus","sex"))
---
+++ acrypt.lua
@@ -0,0 +1,47 @@
+setreadonly(string,false)
+string.startsWith = function(orig,m) 
+    return orig:find(m) == 1
+end  
+string.replace = function(orig,subs,rstr) 
+    return orig:gsub(subs, rstr)
+end
+EncodeA = function(str) 
+    local fields = { str:match( (str:gsub(".", "(.)")) ) }
+    local rel = ""
+    for i,v in pairs(fields) do
+       rel = rel.."/"..tostring(v:byte()) 
+    end
+    return string.reverse(rel)
+  end
+DecodeA = function(str) 
+    local tab = string.split(string.reverse(str), '/')
+    local rel = ""
+    for i,v in pairs(tab) do 
+         if v == "" or v == " " then else
+             rel=rel..string.char(tonumber(v))
+         end
+    end
+    return rel
+ end
+
+EncodeB = function(str)
+        local tmp = EncodeA(str)
+        tmp = "[ACRYPT] "..string.reverse(tmp)
+        return tmp
+end
+
+DecodeB = function(str) 
+    if str:find("%[ACRYPT%] ") == 1 then
+        --print("[DEBUG] Decrypting.")
+        if str:match("[\/]") then 
+           --print("[DEBUG] Decrypting stage 2.")
+            local tmp = string.reverse(str:gsub("%[ACRYPT%] ", ""))
+            return DecodeA(tmp)
+        end
+    end
+end
+local c = EncodeB("BaxoPlenty is cute uwu")
+print(c)
+setclipboard(c)
+print(DecodeB(c))
+ 
